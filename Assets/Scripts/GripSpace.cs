4a227cd2c456085553fb6571594d66566f059053
Luis Abreu <luisandre_69@hotmail.com>
2019-03-30 18:19:41 +0000

    tic tac

---
+++ Assets/Scripts/GripSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GripSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        if (gameController.playerMove == true)
+        {
+            buttonText.text = gameController.GetPlayerSide();
+            button.interactable = false;
+            gameController.EndTurn();
+        }
+    }
+}
